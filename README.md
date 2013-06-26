180f284d4db703b5710678cbcd64581b9b5adaa7
Dewos <dewosmail@gmail.com>
2013-06-26 19:39:49 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+KML Atlas Helper
+======
+
+M-Atlas To KML Converter
+
+Visual Moving Point Parsing
+-------------
+![movingpoint](http://oi42.tinypic.com/2l8b6ok.jpg)
+
+
+TimeStamp Selection Support
+-------------
+![timestamp](http://i41.tinypic.com/4r7saq.png)
+
+
+Automatic "GroupBy-Like" Aggregation on UserId
+-------------
+![userId](http://i40.tinypic.com/33xg394.jpg
---
+++ src/it/unipi/dm2/KMLAtlas.java
@@ -0,0 +1,151 @@
+package it.unipi.dm2;
+
+import de.micromata.opengis.kml.v_2_2_0.ColorMode;
+import de.micromata.opengis.kml.v_2_2_0.Document;
+import de.micromata.opengis.kml.v_2_2_0.Folder;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.KmlFactory;
+import de.micromata.opengis.kml.v_2_2_0.LineString;
+import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import de.micromata.opengis.kml.v_2_2_0.Style;
+import de.micromata.opengis.kml.v_2_2_0.TimeStamp;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.postgis.Geometry;
+import org.postgis.PGgeometry;
+import org.postgis.Point;
+
+/**
+ * Convertitore m-atlas > google earth (kml)
+ * Scritto a casaccio in 3 (+4) ore... bbbbbut it works :)
+ * 
+ * @author Piro Fabio
+ */
+public class KMLAtlas
+{
+    final private static String USER = "postgres";
+    final private static String PASS = "admin";
+    final private static String URL = "jdbc:postgresql://localhost:5432/template_postgis_20";
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException
+    {
+        KMLAtlas.convertMovingPoints("public.traj_complete", new File("paths.kml"));
+    }
+    
+    /**
+     * Export Moving Points into a new KML file
+     * 
+     * @param table any query with m-atlas output (tabl + stats)
+     * @param output any new file
+     * @throws java.sql.SQLException
+     * @throws java.lang.ClassNotFoundException
+     */
+    public static void convertMovingPoints(String table, File output) throws SQLException, ClassNotFoundException
+    {
+        // Create new KML file
+        final Kml kml = KmlFactory.createKml();
+        final Document document = kml.createAndSetDocument();
+        final Folder folder = document.createAndAddFolder().withName(table);
+
+        // Styling
+        final Style style = document.createAndAddStyle().withId("red");
+        style.createAndSetLineStyle().withColorMode(ColorMode.RANDOM).withWidth(1.5);
+
+        // Open Postgress Connection
+        Class.forName("org.postgresql.Driver");
+        Connection conn = DriverManager.getConnection(URL, USER, PASS);
+            
+        // Add the geometry types to the connection. 
+        ((org.postgresql.PGConnection) conn).addDataType("geometry", "org.postgis.PGgeometry");
+        
+        try (Statement statement = conn.createStatement()) 
+        {
+            // Check table and join stats
+            String query = 
+                      " SELECT t.*, s.n_points, s.length, s.duration, s.time_start "
+                    + " FROM " + table + " t, " + table + "_stats s "
+                    + " WHERE t.id = s.id "
+                    + " ORDER BY t.id";
+            
+            ResultSet resultSet = statement.executeQuery(query);
+            Folder userFolder = null;
+            
+            while (resultSet.next()) 
+            {  
+                // Remove M-Atlas prefix for userId
+                String idSplit = resultSet.getString("id").split("_")[0];
+                
+                if(userFolder == null || ! userFolder.getId().startsWith(idSplit))
+                {
+                    // A new forlder for each user
+                    userFolder = folder.createAndAddFolder().withId(idSplit).withName("user: " + idSplit);
+                }
+                
+                // Retrieve data
+                int points = resultSet.getInt("n_points");
+                double length = resultSet.getDouble("length");
+                String duration = resultSet.getString("duration");
+                Date dateStart = resultSet.getDate("time_start");
+                Geometry geom = ((PGgeometry) resultSet.getObject("object")).getGeometry();
+                
+                // @see https://developers.google.com/kml/documentation/kmlreference#timestamp
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                
+                // Only Linestring for paths
+                if (geom.getType() == Geometry.LINESTRING)
+                {
+                    // Add placemark to the map for the path
+                    Placemark pl = userFolder.createAndAddPlacemark()
+                                    .withStyleUrl("#red")
+                                    .withTimePrimitive(new TimeStamp().withWhen(dateFormat.format(dateStart)))
+                                    .withName("#" + geom.numPoints() + " path ("+ points + " points)" )
+                                    .withDescription(
+                                            "Date: " + dateFormat.format(dateStart) + "\n" +
+                                            "Duration: " + duration + " min | " +
+                                            "Lenght: " + Math.round(length) + " mt \n" +
+                                            "UserId:" + idSplit
+                                    );
+
+                        // Starts a new line-path
+                    LineString paths = pl.createAndSetLineString();
+
+                    // Cicles all the points of the founded path
+                    int i = 0;
+                    
+                    for(; i < geom.numPoints(); i++)
+                    {
+                        // Add the next point
+                        Point point = geom.getPoint(i);
+                        paths.addToCoordinates(point.getX() + ", " + point.getY() + ", " + 0 /* point.getZ() */)
+                            .withExtrude(Boolean.TRUE)
+                            .withTessellate(Boolean.TRUE);
+                    }
+                }
+            }
+            
+            // Output the kml file and open it
+            kml.marshal(output);
+            
+            // Stats google earth (warning: rapid develop here. Don't try this at home :)
+            Desktop.getDesktop().open(output);
+        }
+        catch (IOException ex) 
+        {
+            Logger.getLogger(KMLAtlas.class.getName()).log(Level.SEVERE, null, ex);
+        }            
+        finally
+        {
+            conn.close();
+        }
+    }
+}
